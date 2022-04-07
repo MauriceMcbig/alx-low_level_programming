0b085173f4e354a7ce7fb5b807b5355276fd440b
MauriceMcbig <mauriceogbebor@gmail.com>
2022-04-07 14:02:03 -0700

    My radio commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
-/**
- *main - entry point
- *Return: O after printing the function
- */
-int main(void)
-{
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
-}
+#include <stdio.h>                                                                                                                                              
 
+/**                                                                                                                                                             
+
+ *main - entry point                                                                                                                                            
+
+ *Return: O after printing the function                                                                                                                         
+
+ */                                                                                                                                                             
+
+int main(void)                                                                                                                                                  
+
+{                                                                                                                                                               
+
+puts("/"Programming is like building a multilingual puzzle");                                                                                                   
+
+return (0);                                                                                                                                                     
 
