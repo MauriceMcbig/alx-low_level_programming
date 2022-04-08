d523f2e98bd25fbc96f323807770c5770d842d28
MauriceMcbig <mauriceogbebor@gmail.com>
2022-04-08 12:20:49 -0700

    My Zero task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  * 
- * Return: Always 0 (Successs/correct)
+ * Return: Always 0 (Success/correct)
  */
 
 int main(void)
@@ -27,7 +27,7 @@ printf("%i is negative\n", n);
 
 else
 {
-printf("%i is positive\n", n)
+printf("%i is positive\n", n);
 }
 
 return (0);
