ad340ca7fdf248fcf397cba9a4fe4bf27f2f5d3f
Maurice Ogbebor <39393123+MauriceMcbig@users.noreply.github.com>
2022-04-26 14:20:30 +0100

    My 4th commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,20 +10,20 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
-	int r, j;
+	int i, j;
 	int count = 0;
 	char *str1, *str2;
 
 	str1 = s;
 	str2 = accept;
 
-	r = 0;
-	while (str1[r] != '\0') /*Declaring WHILE *s */
+	i = 0;
+	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
 		j = 0;
 		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			if (str2[j] == str1[r]) /*Evaluate condition*/
+			if (str2[j] == str1[i]) /*Evaluate condition*/
 			{
 				count++; /*count number*/
 				break;
@@ -32,12 +32,12 @@ unsigned int _strspn(char *s, char *accept)
 			j++;    /*add j+1*/
 		}
 
-		if (s[r] != accept[j]) /*If aren't equals*/
+		if (s[i] != accept[j]) /*If aren't equals*/
 		{
 			break;
 		}
 
-		r++; /*add x+1*/
+		i++; /*add x+1*/
 	}
 
 	return (count); /*return the value of count*/
